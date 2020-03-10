18bcfffe5fd915b417641f465b8265e55622d26e
Ezeki Zibzibadze <ez@jok.io>
2020-03-10 17:09:14 +0400

    version 9 works

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+})
+export class WelcomeComponent implements OnInit {
+
+  @Input()
+  name: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('aaa');
+
+  }
+
+}
