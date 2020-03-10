994d1b29b3a3890a7dbfaf25b3fe35e21ccd4052
Ezeki Zibzibadze <ez@jok.io>
2020-03-10 17:21:07 +0400

    v8

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss'],
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.scss"]
 })
 export class WelcomeComponent implements OnInit {
-
   @Input()
   name: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    throw new Error('aaa');
-
-  }
-
+  ngOnInit(): void {}
 }
